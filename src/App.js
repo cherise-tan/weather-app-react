dd90557bd9d94e262c5d6454a59459c8554735dc
Cherise <cherisetan@live.com>
2019-09-30 16:32:54 +1300

    Add styling

--- src/App.js
+++ src/App.js
@@ -59,16 +59,28 @@ class App extends React.Component {
   render() {
     return ( 
     <div>
-      <Titles/>
-      <Form getWeather={this.getWeather}/>
-      <Weather 
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}
-        />
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-5 title-container">
+                <Titles/>
+              </div>
+              <div class="col-md-7 form-container">
+                <Form getWeather={this.getWeather}/>  
+                <Weather 
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     );
   }
@@ -76,3 +88,8 @@ class App extends React.Component {
 
 // export 'App' so it can be used elsewhere
 export default App;
+
+
+
+
+
